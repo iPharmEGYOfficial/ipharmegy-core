60748eee440003ad3984b4d0f36b8d83bf0c73be
iPharmEGYOfficial <hithmosama2020@gmail.com>
2026-03-15 13:17:33 +0300

    update

--- server/app.js
+++ server/app.js
@@ -21,7 +21,19 @@ const app = express();
    Middlewares
 ================================ */
 
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://127.0.0.1:3000",
+      "http://localhost:3000",
+      "http://127.0.0.1:5173",
+      "http://localhost:5173"
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    credentials: true
+  })
+);
+
 app.use(express.json());
 
 /* ===============================
